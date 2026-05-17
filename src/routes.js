89b590bd209db550eaa2c78565cc9ec6760881d3
Emmanuel Edache <alexanderedache4@gmail.com>
2026-05-17 23:41:58 +0100

    use & sever validations added

--- src/routes.js
+++ src/routes.js
@@ -1,16 +1,11 @@
 import express from 'express';
 
 import { showHomePage } from './controllers/index.js';
-import { showOrganizationsPage } from './controllers/organizations.js';
-import { showProjectsPage } from './controllers/projects.js';
-import { showCategoriesPage } from './controllers/categories.js';
+import { showOrganizationsPage, showOrganizationDetailsPage, showNewOrganizationForm, processNewOrganizationForm, organizationValidation } from './controllers/organizations.js';
+import { showProjectsPage, showProjectsByOrganizationIdPage, showProjectDetailsPage } from './controllers/projects.js';
+import { showCategoriesPage, showCategoryDetailsPage } from './controllers/categories.js';
 import { testErrorPage } from './controllers/errors.js';
-import { showOrganizationDetailsPage } from './controllers/organizations.js';
-import { showProjectsByOrganizationIdPage } from './controllers/projects.js';
-import { showProjectDetailsPage } from './controllers/projects.js';
-import { showCategoryDetailsPage } from './controllers/categories.js';
-import { showNewOrganizationForm } from './controllers/organizations.js';
-import { processNewOrganizationForm } from './controllers/organizations.js';
+
 const router = express.Router();
 
 router.get('/', showHomePage);
@@ -21,10 +16,13 @@ router.get('/category/:id', showCategoryDetailsPage);
 router.get('/organization/:id', showOrganizationDetailsPage);
 router.get('/projects/organization/:organizationId', showProjectsByOrganizationIdPage);
 router.get('/project/:id', showProjectDetailsPage);
+
 // Route for new organization page
 router.get('/new-organization', showNewOrganizationForm);
+
 // Route to handle new organization form submission
-router.post('/new-organization', processNewOrganizationForm);
+router.post('/new-organization', organizationValidation, processNewOrganizationForm);
+
 // error-handling routes
 router.get('/test-error', testErrorPage);
 
